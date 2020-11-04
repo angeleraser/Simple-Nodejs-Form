2181c244398d5b0ef6c13a16241d453c191b2283
angeleraser <angeleraser@gmail.com>
2020-11-04 15:50:43 -0400

    Update .env API URL variable

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/base/index.css";
 import { App } from "./App";
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App/>, document.getElementById("root"));
